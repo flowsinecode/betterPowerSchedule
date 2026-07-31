5126cb0d524a0a9435d5d9a8c02c379347a0b24c
flowsinecode <flowsine.code@proton.me>
2026-07-31 10:59:29 +0700

    Implement Linux and macOS shutdown scheduling with GUI components

---
+++ core/run/linux.py
@@ -0,0 +1,68 @@
+import subprocess as sb
+import tkinter.messagebox
+import customtkinter as ctk
+
+def stop():
+    password = ctk.CTkInputDialog(text="Because you're using Linux, you need to enter your password to continue. We promise not to send your password to us!", title="Enter password").get_input()
+    if password is not None:
+        execute = sb.Popen(['sudo', '-S', 'shutdown', '-c'],
+            stdin=sb.PIPE, 
+            stdout=sb.PIPE, 
+            stderr=sb.PIPE,
+            shell=False,
+            text=True)
+        stdout, stderr = execute.communicate(input=f"{password}\n")
+        if execute.returncode != 0:
+            tkinter.messagebox.showerror("Error", f"{stderr}")
+        else:
+            tkinter.messagebox.showinfo("Cancelled", "Schedule successfully cancelled")
+
+def start(time, action, comment):
+    password = ctk.CTkInputDialog(text="Because you're using Linux, you need to enter your password to continue. We promise not to send your password to us!", title="Enter password").get_input()
+    if password is not None:
+        if comment == '':
+            if action =='Shut down':
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '+'+str(time)],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
+            else:
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '-r', '+'+str(time)],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
+        else:
+            if action =='Shut down':
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '+'+str(time), comment],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True,)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
+            else:
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '-r', '+'+str(time), comment],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
--- core/run/macos.py
+++ core/run/macos.py
@@ -4,49 +4,65 @@ import customtkinter as ctk
 
 def stop():
     password = ctk.CTkInputDialog(text="Because you're using macOS, you need to enter your password to continue. We promise not to send your password to us!", title="Enter password").get_input()
-    execute = sb.Popen(['sudo', 'killall', 'shutdown'],
-        stdin=sb.PIPE, 
-        stdout=sb.PIPE, 
-        stderr=sb.PIPE,
-        shell=False,
-        text=True)
-    stdout, stderr = execute.communicate(input=f"{password}\n")
-    if execute.returncode != 0:
-        tkinter.messagebox.showerror("Error", f"{stderr}")
-    else:
-        tkinter.messagebox.showinfo("Cancelled", "Schedule successfully cancelled")
+    if password is not None:
+        execute = sb.Popen(['sudo', '-S', 'killall', 'shutdown'],
+            stdin=sb.PIPE, 
+            stdout=sb.PIPE, 
+            stderr=sb.PIPE,
+            shell=False,
+            text=True)
+        stdout, stderr = execute.communicate(input=f"{password}\n")
+        if execute.returncode != 0:
+            tkinter.messagebox.showerror("Error", f"{stderr}")
+        else:
+            tkinter.messagebox.showinfo("Cancelled", "Schedule successfully cancelled")
 
 def start(time, action, comment):
     password = ctk.CTkInputDialog(text="Because you're using macOS, you need to enter your password to continue. We promise not to send your password to us!", title="Enter password").get_input()
-    if comment == '':
-        if action =='Shut down':
-            execute = sb.Popen(['sudo', 'shutdown', '-h', '+'+str(time)],
-                    stdin=sb.PIPE, 
-                    stdout=sb.PIPE, 
-                    stderr=sb.PIPE,
-                    text=True)
+    if password is not None:
+        if comment == '':
+            if action =='Shut down':
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '-h', '+'+str(time)],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
+            else:
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '-r', '+'+str(time)],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
         else:
-            execute = sb.Popen(['sudo', 'shutdown', '-r', '+'+str(time)],
-                    stdin=sb.PIPE, 
-                    stdout=sb.PIPE, 
-                    stderr=sb.PIPE,
-                    text=True)
-    else:
-        if action =='Shut down':
-            execute = sb.Popen(['sudo', 'shutdown', '-h', '+'+str(time), comment],
-                    stdin=sb.PIPE, 
-                    stdout=sb.PIPE, 
-                    stderr=sb.PIPE,
-                    text=True,)
-        else:
-            execute = sb.Popen(['sudo', 'shutdown', '-r', '+'+str(time), comment],
-                    stdin=sb.PIPE, 
-                    stdout=sb.PIPE, 
-                    stderr=sb.PIPE,
-                    text=True)
-    
-    stdout, stderr = execute.communicate(input=f"{password}\n")
-    if execute.returncode != 0:
-        tkinter.messagebox.showerror("Error", f"{stderr}")
-    else:
-        tkinter.messagebox.showinfo("Started", "Schedule successfully started")
+            if action =='Shut down':
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '-h', '+'+str(time), comment],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True,)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
+            else:
+                execute = sb.Popen(['sudo', '-S', 'shutdown', '-r', '+'+str(time), comment],
+                        stdin=sb.PIPE, 
+                        stdout=sb.PIPE, 
+                        stderr=sb.PIPE,
+                        text=True)
+                stdout, stderr = execute.communicate(input=f"{password}\n")
+                if execute.returncode != 0:
+                    tkinter.messagebox.showerror("Error", f"{stderr}")
+                else:
+                    tkinter.messagebox.showinfo("Started", "Schedule successfully started")
